abde763f750433584226295f5d2b7cbcbbef18ca
Kelsey Williams <kelseywilliams1200@gmail.com>
2023-10-08 21:26:02 -0700

    lab1

---
+++ lab1/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int test(int n)
+{
+	return (n % 10);
+}
+int main(int argc, char *argv[]) {
+	int i = 294;
+	printf("The digit in the ones place of %d is %d\n", i, test(i));
+	return 0;
+}
